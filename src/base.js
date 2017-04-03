0ab4ffa9d4ec8594f98829fb44c08aa1a7adc057
Stan van Sluisveld <stan.sluisveld@gmail.com>
2017-04-03 15:01:39 +0200

    vue setup

---
+++ src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyADmJoeITLryUSGq1Qu6fIUgos1vKgbAFE",
+    authDomain: "catch-of-the-day-stan-sluis.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-stan-sluis.firebaseio.com",
+
+});
+
+export default base;
