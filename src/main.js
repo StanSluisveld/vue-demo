85cd54090bd5f905c3e82c015fe8ca58fe92d3e7
Stan van Sluisveld <stan.sluisveld@gmail.com>
2017-04-11 14:59:29 +0200

    Deleted router

--- src/main.js
+++ src/main.js
@@ -1,27 +1,12 @@
 import Vue from 'vue';
 import './css/style.css';
-import VueRouter from 'vue-router';
 
-import App from './components/App';
 import Store from './components/Store';
 import StorePicker from './components/StorePicker';
 import NotFound from './components/NotFound';
 
-Vue.use(VueRouter);
-
-const routes = [
-    {path: '/', component: StorePicker},
-    {path: '/store', component: Store}
-
-];
-
-const router = new VueRouter({
-    routes
-});
-
 new Vue({
     el: '#main',
-    router,
     render: h => h(Store)
 })
 
